38fb1310db925a12465e4f80f452abb29ae8e28d
KhalidRedwanSun <khalidredwan701@gmail.com>
2024-08-27 14:44:04 +0600

    Next-Project-Section Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'next-projectBg': "url('/assets/bg.jpg')",
+      },
+    },
   },
   plugins: [],
 };
